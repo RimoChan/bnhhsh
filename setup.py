4424a3162105ec5dd2dde915cb8aa2411338afcd
RimoChan <RimoChan@stu.xmu.edu.cn>
2021-08-21 15:11:25 +0800

    第一次提交

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+setuptools.setup(
+    name='bnhhsh',
+    version='1.0.0',
+    author='RimoChan',
+    author_email='the@librian.net',
+    description='bnhhsh',
+    url='https://github.com/RimoChan/bnhhsh',
+    packages=['bnhhsh'],
+    package_data={
+        'bnhhsh': ['data/色情词库.json', 'data/莉沫词库.json', 'data/常用汉字.json', 'data/现代汉语常用词表.json'],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'pypinyin>=0.42.0',
+    ],
+    python_requires='>=3.8',
+)
