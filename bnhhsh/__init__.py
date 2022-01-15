2e189828c98179b0aa104ab9fcdf0b27c83f11c1
RimoChan <RimoChan@stu.xmu.edu.cn>
2022-01-15 18:09:52 +0800

    好

--- bnhhsh/__init__.py
+++ bnhhsh/__init__.py
@@ -41,10 +41,10 @@ def 破处():
     n = max(词桶)
     for i in range(1, n+1):
         词桶.setdefault(i, {})
-    with open(Path(tempfile.gettempdir())/'bnhhsh词桶v1.2.1.pkl', 'wb') as f:
+    with open(Path(tempfile.gettempdir())/'bnhhsh词桶v1.2.2.pkl', 'wb') as f:
         pickle.dump(词桶, f)
 
-q = Path(Path(tempfile.gettempdir())/'bnhhsh词桶v1.2.1.pkl')
+q = Path(Path(tempfile.gettempdir())/'bnhhsh词桶v1.2.2.pkl')
 if not q.is_file():
     破处()
 with open(q, 'rb') as f:
